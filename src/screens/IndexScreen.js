4f333a7f5f2e231cbd53773b193185bca8427433
Nathan Furman <ncfusn@gmail.com>
2020-08-15 15:36:20 +0300

    StackNavigator wired

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import { StyleSheet, View, Button, Text } from 'react-native';
+
+const IndexScreen = () => {
+    return <View>
+        <Text>IndexScreen</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    //
+})
+
+export default IndexScreen
